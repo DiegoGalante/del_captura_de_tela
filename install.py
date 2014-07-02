30b482d139eda25dd7e33f78aa97199acc5b4b82
Diego Galante <diegogalantte@gmail.com>
2014-07-02 19:07:10 -0300

    Update install.py

--- install.py
+++ install.py
@@ -2,6 +2,11 @@
 # -*- coding: utf-8 -*-
 
 import os
+import subprocess
+
+import del_captura_de_tela
+import agendar_crontab
+
 
 try:
 	pasta = os.environ["HOME"]
@@ -18,25 +23,24 @@ try:
 				if 'script' not in diretorio:
 					try:
 						os.system('sudo mkdir /etc/script/')
-						print '\n########################################################################'
-						print '# Diretório criado com sucesso!                                        #'
-								
+						print 'Diretório criado com sucesso!'
+
 						diretorio = os.listdir('/etc/')
 						if 'script' in diretorio:
 							try:
 								os.system('cd $HOME/Downloads/del_captura_de_tela-master/ && sudo mv del_captura_de_tela.py /etc/script/')
-								print '# Arquivo adicionado na pasta com sucesso! 	  		       #'
+								print 'Arquivo adicionado na pasta com sucesso!'
 
 								diretorio_script = os.listdir('/etc/script/')
 								if 'del_captura_de_tela.py' in diretorio_script:
 									try:
 										os.system('cd $HOME/Downloads/ && rm -r del_captura_de_tela-master/')
-										print '# Arquivo atualizado ..                                                #'
-										
+										print 'Arquivo atualizado com sucesso!\n'
+
 										user = pasta.split('/')
 										diretorio_etc = os.listdir("/etc/")
 										comando = "*/1 * * * * "+user[2]+" python /etc/script/del_captura_de_tela.py"
-										
+
 										if 'crontab' in diretorio_etc:
 											try:
 												os.system("sudo cp /etc/crontab /etc/crontab-script")
@@ -55,66 +59,63 @@ try:
 													myfile.write("#\n")
 													myfile.close()
 													ok = True
-													
 
-												if ok:
-													os.system("sudo cp /etc/crontab-script /etc/crontab")
-													os.system("sudo rm /etc/crontab-script")
-													os.system("sudo rm /etc/crontab-backup")
-													print '# Crontab agendado no sistema com sucesso!                             #'
-													print '# Instalação do script finalizada!                                     #'
-													print '########################################################################'
-												else:
-													os.system("sudo cp /etc/crontab-backup /etc/crontab")
-													os.system("sudo rm /etc/crontab-script")
-													os.system("sudo rm /etc/crontab-backup")
-													print '\nComando já agendado no sistema. Arquivo não atualizado!\n'
-
-											except Exception, e:
-												print 'Não foi possível agendar o arquivo no sistema!'
-										
-									except Exception, e:
-										print 'Não foi possível deletar a pasta del_captura_de_tela-master/ em $HOME/Downloads'
-
-								else:
-									try:
-										os.system('cd $HOME/Downloads/ && rm -r del_captura_de_tela-master/')
-							
-										user = pasta.split('/')
-										diretorio_etc = os.listdir("/etc/")
-										comando = "*/1 * * * * "+user[2]+" python /etc/script/del_captura_de_tela.py"
-										
-										if 'crontab' in diretorio_etc:
-											try:
-												os.system("sudo cp /etc/crontab /etc/crontab-script")
-												os.system("sudo cp /etc/crontab /etc/crontab-backup")
-												os.system("sudo chmod 777 /etc/crontab-script")
-												os.system("sudo chown diego:diego /etc/crontab-script")
-
-												myfile = open('/etc/crontab-script', 'r+a')
-												line = myfile.read()
-												words = line.split("#")
-												words = line.split("\n")
-
-												if comando not in words:
-													myfile.write("\n#Executa script de deletar screenshots.\n")
-													myfile.write(comando+"\n")
-													myfile.write("#\n")
-													myfile.close()
-													ok = True
-													
 
 												if ok:
 													os.system("sudo cp /etc/crontab-script /etc/crontab")
 													os.system("sudo rm /etc/crontab-script")
 													os.system("sudo rm /etc/crontab-backup")
 													print 'Crontab agendado no sistema com sucesso!'
-													print '\nInstalação do script com sucesso!'
 												else:
 													os.system("sudo cp /etc/crontab-backup /etc/crontab")
 													os.system("sudo rm /etc/crontab-script")
 													os.system("sudo rm /etc/crontab-backup")
-													print '\nComando já agendado no sistema. Arquivo não atualizado!\n'
+													print 'Comando já agendado no sistema. Arquivo não atualizado!\n'
+
+											except Exception, e:
+												print 'Não foi possível agendar o arquivo no sistema!'
+
+									except Exception, e:
+										print 'Não foi possível deletar a pasta del_captura_de_tela-master/ em $HOME/Downloads'
+
+								else:
+									try:
+										os.system('cd $HOME/Downloads/ && rm -r del_captura_de_tela-master/')
+
+										user = pasta.split('/')
+										diretorio_etc = os.listdir("/etc/")
+										comando = "*/1 * * * * "+user[2]+" python /etc/script/del_captura_de_tela.py"
+
+										if 'crontab' in diretorio_etc:
+											try:
+												os.system("sudo cp /etc/crontab /etc/crontab-script")
+												os.system("sudo cp /etc/crontab /etc/crontab-backup")
+												os.system("sudo chmod 777 /etc/crontab-script")
+												os.system("sudo chown diego:diego /etc/crontab-script")
+
+												myfile = open('/etc/crontab-script', 'r+a')
+												line = myfile.read()
+												words = line.split("#")
+												words = line.split("\n")
+
+												if comando not in words:
+													myfile.write("\n#Executa script de deletar screenshots.\n")
+													myfile.write(comando+"\n")
+													myfile.write("#\n")
+													myfile.close()
+													ok = True
+
+
+												if ok:
+													os.system("sudo cp /etc/crontab-script /etc/crontab")
+													os.system("sudo rm /etc/crontab-script")
+													os.system("sudo rm /etc/crontab-backup")
+													print 'Crontab agendado no sistema com sucesso!'
+												else:
+													os.system("sudo cp /etc/crontab-backup /etc/crontab")
+													os.system("sudo rm /etc/crontab-script")
+													os.system("sudo rm /etc/crontab-backup")
+													print 'Comando já agendado no sistema. Arquivo não atualizado!\n'
 
 											except Exception, e:
 												print 'Não foi possível agendar o arquivo no sistema!'
@@ -124,28 +125,22 @@ try:
 
 
 							except Exception, e:
-								pass#print 'Não foi possível mover o arquivo da pasta $HOME/Downloads/del_captura_de_tela-master/ '
+								print 'Não foi possível mover o arquivo da pasta $HOME/Downloads/del_captura_de_tela-master/ '
 						else:
 							pass
 							#print 'Arquivo del não existe ema /etc/script/'
 					except Exception, e:
 						print 'Não foi possível criar a pasta script/ em /etc/'
 				else:
-					print '\n########################################################################'
-					print '# Diretório já criado, atualizando arquivos..                          #'
 					diretorio_script = os.listdir('/etc/script/')
-					if 'del_captura_de_tela.py' in diretorio_script:
+					if 'del_captura_de_tela.py' not in diretorio_script:
 						try:
-							os.system('cd $HOME/Downloads/del_captura_de_tela-master/ && sudo mv del_captura_de_tela.py /etc/script/')
-							print '# Arquivo atualizado com sucesso! 	  		               #'
-							print '# Verificando Crontab .. 	  		                       #'
-
 							os.system('cd $HOME/Downloads/ && rm -r del_captura_de_tela-master/')
-							
+
 							user = pasta.split('/')
 							diretorio_etc = os.listdir("/etc/")
 							comando = "*/1 * * * * "+user[2]+" python /etc/script/del_captura_de_tela.py"
-									
+
 							if 'crontab' in diretorio_etc:
 								try:
 									os.system("sudo cp /etc/crontab /etc/crontab-script")
@@ -164,26 +159,67 @@ try:
 										myfile.write("#\n")
 										myfile.close()
 										ok = True
-									else:
-										print '# Comando já agendado no sistema!    	  	                       #'
-										print '########################################################################'
 
 
-									if ok:
-										os.system("sudo cp /etc/crontab-script /etc/crontab")
-										os.system("sudo rm /etc/crontab-script")
-										os.system("sudo rm /etc/crontab-backup")
-										print 'Crontab agendado no sistema com sucesso!'
-										print '\nInstalação do script com sucesso!'
-									else:
-										os.system("sudo cp /etc/crontab-backup /etc/crontab")
-										os.system("sudo rm /etc/crontab-script")
-										os.system("sudo rm /etc/crontab-backup")
-										print '\nComando já agendado no sistema. Arquivo não atualizado!\n'
+										if ok:
+											os.system("sudo cp /etc/crontab-script /etc/crontab")
+											os.system("sudo rm /etc/crontab-script")
+											os.system("sudo rm /etc/crontab-backup")
+											print 'Crontab agendado no sistema com sucesso!'
+										else:
+											os.system("sudo cp /etc/crontab-backup /etc/crontab")
+											os.system("sudo rm /etc/crontab-script")
+											os.system("sudo rm /etc/crontab-backup")
+											print 'Comando já agendado no sistema. Arquivo não atualizado!\n'
 
 								except Exception, e:
-									pass#print 'Não foi possível agendar o arquivo no sistema!'
+									print 'Não foi possível agendar o arquivo no sistema!'
 
+						except Exception, e:
+							pass
+							#print 'Não foi possível deletar a pasta del_captura_de_tela-master/ em $HOME/Downloads'
+
+					else:
+						try:
+							os.system('cd $HOME/Downloads/ && rm -r del_captura_de_tela-master/')
+
+							user = pasta.split('/')
+							diretorio_etc = os.listdir("/etc/")
+							comando = "*/1 * * * * "+user[2]+" python /etc/script/del_captura_de_tela.py"
+
+							if 'crontab' in diretorio_etc:
+								try:
+									os.system("sudo cp /etc/crontab /etc/crontab-script")
+									os.system("sudo cp /etc/crontab /etc/crontab-backup")
+									os.system("sudo chmod 777 /etc/crontab-script")
+									os.system("sudo chown diego:diego /etc/crontab-script")
+
+									myfile = open('/etc/crontab-script', 'r+a')
+									line = myfile.read()
+									words = line.split("#")
+									words = line.split("\n")
+
+									if comando not in words:
+										myfile.write("\n#Executa script de deletar screenshots.\n")
+										myfile.write(comando+"\n")
+										myfile.write("#\n")
+										myfile.close()
+										ok = True
+
+
+										if ok:
+											os.system("sudo cp /etc/crontab-script /etc/crontab")
+											os.system("sudo rm /etc/crontab-script")
+											os.system("sudo rm /etc/crontab-backup")
+											print 'Crontab agendado no sistema com sucesso!'
+										else:
+											os.system("sudo cp /etc/crontab-backup /etc/crontab")
+											os.system("sudo rm /etc/crontab-script")
+											os.system("sudo rm /etc/crontab-backup")
+											print 'Comando já agendado no sistema. Arquivo não atualizado!\n'
+
+								except Exception, e:
+									print 'Não foi possível agendar o arquivo no sistema!'
 						except Exception, e:
 							pass
 							#print 'Não foi possível deletar a pasta del_captura_de_tela-master/ em $HOME/Downloads'
@@ -192,10 +228,224 @@ try:
 				print 'Não foi possível descompactar o arquivo.'
 
 		else:
-			print 'Arquivo del_captura_de_tela-master.zip não existe em $HOME/Downloads/'
+			#print 'Arquivo del_captura_de_tela-master.zip não existe em $HOME/Downloads/'
+			pass
+	else:
+		diretorio = os.listdir('/etc/')
+		if 'script' not in diretorio:
+			try:
+				os.system('sudo mkdir /etc/script/')
+				print 'Diretório criado com sucesso!'
 
-	print '\n--> Lembre-se de compartilhar esse arquivo com alguem que tem o mesmo problema. (:'
-	print "--> Em breve será feito melhorias neste arquivo, fiquem ligados. Enjoy!\n"
+				diretorio = os.listdir('/etc/')
+				if 'script' in diretorio:
+					try:
+						os.system('cd $HOME/Downloads/del_captura_de_tela-master/ && sudo mv del_captura_de_tela.py /etc/script/')
+						print 'Arquivo adicionado na pasta com sucesso!'
+
+						diretorio_script = os.listdir('/etc/script/')
+						if 'del_captura_de_tela.py' in diretorio_script:
+							try:
+								os.system('cd $HOME/Downloads/ && rm -r del_captura_de_tela-master/')
+								print 'Instalação dos arquivos concluída com sucesso!\n'
+
+								user = pasta.split('/')
+								diretorio_etc = os.listdir("/etc/")
+								comando = "*/1 * * * * "+user[2]+" python /etc/script/del_captura_de_tela.py"
+
+								if 'crontab' in diretorio_etc:
+									try:
+										os.system("sudo cp /etc/crontab /etc/crontab-script")
+										os.system("sudo cp /etc/crontab /etc/crontab-backup")
+										os.system("sudo chmod 777 /etc/crontab-script")
+										os.system("sudo chown diego:diego /etc/crontab-script")
+
+										myfile = open('/etc/crontab-script', 'r+a')
+										line = myfile.read()
+										words = line.split("#")
+										words = line.split("\n")
+
+										if comando not in words:
+											myfile.write("\n#Executa script de deletar screenshots.\n")
+											myfile.write(comando+"\n")
+											myfile.write("#\n")
+											myfile.close()
+											ok = True
+
+
+											if ok:
+												os.system("sudo cp /etc/crontab-script /etc/crontab")
+												os.system("sudo rm /etc/crontab-script")
+												os.system("sudo rm /etc/crontab-backup")
+												print 'Crontab agendado no sistema com sucesso!'
+											else:
+												os.system("sudo cp /etc/crontab-backup /etc/crontab")
+												os.system("sudo rm /etc/crontab-script")
+												os.system("sudo rm /etc/crontab-backup")
+												print 'Comando já agendado no sistema. Arquivo não atualizado!\n'
+
+									except Exception, e:
+										print 'Não foi possível agendar o arquivo no sistema!'
+
+							except Exception, e:
+								print 'Não foi possível deletar a pasta del_captura_de_tela-master/ em $HOME/Downloads'
+						else:
+							try:
+								os.system('cd $HOME/Downloads/ && rm -r del_captura_de_tela-master/')
+
+								print 'Instalação dos arquivos concluída com sucesso!\n'
+
+								user = pasta.split('/')
+								diretorio_etc = os.listdir("/etc/")
+								comando = "*/1 * * * * "+user[2]+" python /etc/script/del_captura_de_tela.py"
+
+								if 'crontab' in diretorio_etc:
+									try:
+										os.system("sudo cp /etc/crontab /etc/crontab-script")
+										os.system("sudo cp /etc/crontab /etc/crontab-backup")
+										os.system("sudo chmod 777 /etc/crontab-script")
+										os.system("sudo chown diego:diego /etc/crontab-script")
+
+										myfile = open('/etc/crontab-script', 'r+a')
+										line = myfile.read()
+										words = line.split("#")
+										words = line.split("\n")
+
+										if comando not in words:
+											myfile.write("\n#Executa script de deletar screenshots.\n")
+											myfile.write(comando+"\n")
+											myfile.write("#\n")
+											myfile.close()
+											ok = True
+
+
+											if ok:
+												os.system("sudo cp /etc/crontab-script /etc/crontab")
+												os.system("sudo rm /etc/crontab-script")
+												os.system("sudo rm /etc/crontab-backup")
+												print 'Crontab agendado no sistema com sucesso!'
+											else:
+												os.system("sudo cp /etc/crontab-backup /etc/crontab")
+												os.system("sudo rm /etc/crontab-script")
+												os.system("sudo rm /etc/crontab-backup")
+												print 'Comando já agendado no sistema. Arquivo não atualizado!\n'
+
+									except Exception, e:
+										print 'Não foi possível agendar o arquivo no sistema!'
+
+							except Exception, e:
+								pass
+
+
+					except Exception, e:
+						pass
+				else:
+					pass
+					#print 'Arquivo del não existe ema /etc/script/'
+			except Exception, e:
+				print 'Não foi possível criar a pasta script/ em /etc/'
+
+		else:
+			try:
+
+				os.system('cd $HOME/Downloads/del_captura_de_tela-master/ && sudo mv del_captura_de_tela.py /etc/script/')
+				print 'Arquivo adicionado na pasta com sucesso!'
+
+				diretorio_script = os.listdir('/etc/script/')
+				if 'del_captura_de_tela.py' in diretorio_script:
+					try:
+						os.system('cd $HOME/Downloads/ && rm -r del_captura_de_tela-master/')
+
+						print 'Arquivo atualizado com sucesso!\n'
+
+						user = pasta.split('/')
+						diretorio_etc = os.listdir("/etc/")
+						comando = "*/1 * * * * "+user[2]+" python /etc/script/del_captura_de_tela.py"
+
+						if 'crontab' in diretorio_etc:
+							try:
+								os.system("sudo cp /etc/crontab /etc/crontab-script")
+								os.system("sudo cp /etc/crontab /etc/crontab-backup")
+								os.system("sudo chmod 777 /etc/crontab-script")
+								os.system("sudo chown diego:diego /etc/crontab-script")
+
+								myfile = open('/etc/crontab-script', 'r+a')
+								line = myfile.read()
+								words = line.split("#")
+								words = line.split("\n")
+
+								if comando not in words:
+									myfile.write("\n#Executa script de deletar screenshots.\n")
+									myfile.write(comando+"\n")
+									myfile.write("#\n")
+									myfile.close()
+									ok = True
+
+									if ok:
+										os.system("sudo cp /etc/crontab-script /etc/crontab")
+										os.system("sudo rm /etc/crontab-script")
+										os.system("sudo rm /etc/crontab-backup")
+										print 'Crontab agendado no sistema com sucesso!'
+									else:
+										os.system("sudo cp /etc/crontab-backup /etc/crontab")
+										os.system("sudo rm /etc/crontab-script")
+										os.system("sudo rm /etc/crontab-backup")
+										print 'Comando já agendado no sistema. Arquivo não atualizado!\n'
+
+							except Exception, e:
+								print 'Não foi possível agendar o arquivo no sistema!'
+
+					except Exception, e:
+						pass
+
+				else:
+					try:
+						os.system('cd $HOME/Downloads/ && rm -r del_captura_de_tela-master/')
+
+						print 'Instalação dos arquivos concluída com sucesso!\n'
+
+						user = pasta.split('/')
+						diretorio_etc = os.listdir("/etc/")
+						comando = "*/1 * * * * "+user[2]+" python /etc/script/del_captura_de_tela.py"
+
+						if 'crontab' in diretorio_etc:
+							try:
+								os.system("sudo cp /etc/crontab /etc/crontab-script")
+								os.system("sudo cp /etc/crontab /etc/crontab-backup")
+								os.system("sudo chmod 777 /etc/crontab-script")
+								os.system("sudo chown diego:diego /etc/crontab-script")
+
+								myfile = open('/etc/crontab-script', 'r+a')
+								line = myfile.read()
+								words = line.split("#")
+								words = line.split("\n")
+
+								if comando not in words:
+									myfile.write("\n#Executa script de deletar screenshots.\n")
+									myfile.write(comando+"\n")
+									myfile.write("#\n")
+									myfile.close()
+									ok = True
+
+
+									if ok:
+										os.system("sudo cp /etc/crontab-script /etc/crontab")
+										os.system("sudo rm /etc/crontab-script")
+										os.system("sudo rm /etc/crontab-backup")
+										print 'Crontab agendado no sistema com sucesso!'
+									else:
+										os.system("sudo cp /etc/crontab-backup /etc/crontab")
+										os.system("sudo rm /etc/crontab-script")
+										os.system("sudo rm /etc/crontab-backup")
+										print 'Comando já agendado no sistema. Arquivo não atualizado!\n'
+
+							except Exception, e:
+								print 'Não foi possível agendar o arquivo no sistema!'
+					except Exception, e:
+						pass
+
+			except Exception, e:
+						print 'Não foi possível mover o arquivo da pasta $HOME/Downloads/del_captura_de_tela-master/ '
 
 except Exception, e:
 	print 'Falha crítica! Informe o criador do script ou algum programador que possa resolver.'
